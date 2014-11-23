773db65f65b3e9ad3f1810d8e4d072319dd6a67f
Louis Spinelli <KnowWhatsKnown@gmail.com>
2014-11-23 17:02:28 -0600

    Testing: Radio Reciever

--- messageFromJamUnar/messageFromJamUnar.ino
+++ messageFromJamUnar/messageFromJamUnar.ino
@@ -10,39 +10,45 @@
     
     //  Must Initialize nd name variables and constants
     
-    SoftwareSerial rfSerial315(2, 1);      // rx, tx 
-    SoftwareSerial rfSerial433(3, 1);      // rx, tx
+    //SoftwareSerial rfSerial315(2, 1);      // rx, tx 
+    //SoftwareSerial rfSerial433(0, 1);      // rx, tx
     
-    int rDistance;
-    int lDistance;
+    //int rDistance;
+    //int lDistance;
+    int incomingByte = 0;
     
     // setup the pins and baud rates
     
     void setup()
     {
       // Open serial communication to the computer
-      Serial.begin(9600);
+      Serial.begin(2400);
       // set the data rate for the SoftwareSerial port
-      rfSerial315.begin(2400);
-      rfSerial433.begin(4800);
+      //rfSerial315.begin(2400);
+      //rfSerial433.begin(2400);
     }
     
     // Now it is time to setup a loop
     void loop()
     {
-      receiveData();
+      //receiveData();
       
       // Here is a test to check if info is received
       
-      Serial.println(rDistance, lDistance);
-      delay(500);
+     // Serial.println(rDistance, lDistance);
+      //delay(500);
+      if (Serial.available() > 0) {
+        incomingByte = Serial.read();
+        Serial.println(incomingByte, DEC);
+        }
+      incomingByte = 0;
     }
     
-    void receiveData()
-    {
-      rDistance = rfSerial315.read();
-      lDistance = rfSerial433.read();
-    }
+   // void receiveData()
+    //{
+    //  rDistance = rfSerial315.read();
+     // lDistance = rfSerial433.read();
+    //}
     
     
       
